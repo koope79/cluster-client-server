4750c4d6a2318aecc1c0dea824458cdc81290e67
koope79 <70471003+koope79@users.noreply.github.com>
2022-03-31 16:25:25 +0300

    push

---
+++ scheduler.py
@@ -0,0 +1,112 @@
+# Планировщик - основной связующий элемент. Знает информацию о свободных и использующихся портах, а так же какие клиенты
+# их используют
+# TODO сделать мапу для восстановления после сбоя ноды
+import argparse
+import sys
+import logging
+import os
+import socket
+import server
+import multiprocessing
+
+file_log = logging.FileHandler("scheduler.log")
+console_out = logging.StreamHandler()
+
+logging.basicConfig(handlers=(file_log, console_out),
+                    format='[%(asctime)s | %(levelname)s | SCHEDULER]: %(message)s',
+                    datefmt='%m.%d.%Y %H:%M:%S',
+                    level=logging.INFO)
+
+# TODO сделать массив
+manager = multiprocessing.Manager()
+ports = []
+free_ports = manager.list()
+# информация, на каком портц какой клиент сидит
+# TODO Manager
+port_client = {}
+condition = multiprocessing.Condition()
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', help='path to file with hosts')
+    parser.add_argument("-c", "--config", help='path to config file')
+    parser.add_argument("-p", "--port", help='count of ports in pull', default=2)
+    return parser
+
+
+def run_clients(hosts: str, config: str):
+    try:
+        logging.info("Run clients...")
+        host_file = open(hosts)
+        config_file = open(config)
+        path_to_client_file = config_file.readline().replace("\n", "")
+        for line in host_file:
+            os.system('python3 {}'.format(path_to_client_file))
+            logging.info("{} running!".format(line.replace('\n', '')))
+    except:
+        logging.error("error while create workers")
+
+
+# вечное прослушаваие порта в одном экземпляре, но в отдельном процессе
+
+def server_port_listen(data, conn, addr, hosts, config):
+    global free_ports
+    # сигнал от сервера, что процессы на портах создались,
+    # можно запускать рабочих
+    if data.decode() == 'port_created':
+        run_clients(hosts, config)
+
+    if data.decode() == 'return_port':
+        conn.sendall('ready'.encode())
+        port_number = conn.recv(4)
+        condition.acquire()
+        free_ports.append(int.from_bytes(port_number, 'big'))
+        logging.info("Count of free ports {}".format(len(free_ports)))
+        # рассылаем сообщение о поступлении нового свободного порта
+        condition.notify()
+        condition.release()
+
+
+def port_listen(func, main_port, hosts, config):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', main_port))
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        logging.info("Connected: " + str(addr))
+        while True:
+            data = conn.recv(1024)
+
+            logging.info("Command " + data.decode() + " from " + str(addr))
+
+            if not data:
+                break
+
+            func(data, conn, addr, hosts, config)
+
+        logging.info("Connection with " + str(addr) + " will be closed")
+        conn.close()
+
+
+# TODO сделать местный kill
+def start_scheduler(hosts: str, config: str):
+    server_port_listen_process = multiprocessing.Process(target=port_listen,
+                                                         args=(server_port_listen, 9090, hosts, config))
+    server_port_listen_process.start()
+    server.start_server(ports)
+    # рабочие запускаются после сигнала сервера
+
+
+if __name__ == '__main__':
+    # global free_ports
+    parser = create_parser()
+    args = parser.parse_args(sys.argv[1:])
+    # инициализируем возможные порты
+    start_port = 9091
+    for i in range(0, args.port):
+        ports.append(start_port)
+        # изначально все порты свободны
+        free_ports.append(start_port)
+        start_port += 1
+    start_scheduler(args.host, args.config)
---
+++ server.py
@@ -0,0 +1,104 @@
+# Сервер - держатель общих данных, он их отдает и сохраняет процессобезопасно
+import os
+import logging
+import socket
+import multiprocessing
+
+file_log = logging.FileHandler("server.log")
+console_out = logging.StreamHandler()
+
+logging.basicConfig(handlers=(file_log, console_out),
+                    format='[%(asctime)s | %(levelname)s | SERVER]: %(message)s',
+                    datefmt='%m.%d.%Y %H:%M:%S',
+                    level=logging.INFO)
+
+ports = []
+
+scheduler_port = 9091
+
+
+# общие данные
+manager = multiprocessing.Manager()
+# TODO fix it
+result = manager.list([0])
+# TODO fix it
+free_images = manager.list([0])
+shapes = manager.list()
+
+# для каждой операции свой лок, т.к. общие данные не пересакаются в разных операциях
+returnLock = multiprocessing.Lock()
+getLock = multiprocessing.Lock()
+
+
+def start_server(_ports):
+    # записываем порты
+    global free_images
+    global shapes
+    ports = _ports
+    listen(ports)
+
+
+# основной обработчик задач от клиентов
+def listen_process(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((ip, port))
+    logging.info("Creating_server_ports..")
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        logging.info("Connected: " + str(addr))
+        while True:
+            global result
+            global lock
+
+            data = conn.recv(1024)
+
+            logging.info("SERVER Command " + data.decode() + " from " + str(addr))
+
+            if not data:
+                logging.error("Connection with " + str(addr) + " will be closed. Data not found")
+                break
+            
+            if data.decode() == 'get':
+                print("send file")
+                break
+
+        logging.info("Connection with " + str(addr) + " will be closed")
+        close_port(port, conn)
+
+
+def close_port(port, conn):
+    port = port
+    conn.close()
+    sock = socket.socket()
+    sock.connect(('', scheduler_port))
+    sock.send('return_port'.encode())
+    resp = sock.recv(5)
+    # TODO а если не ready
+    logging.info("Scheduler answer: {}".format(resp.decode()))
+    if resp.decode() == 'ready':
+        sock.send(port.to_bytes(3, 'big'))
+    sock.close()
+
+
+# запускаем на каждом порту в пуле прослушивание. Каждый порт слушает в отдельном процессе
+def listen(ports):
+    logging.info("Listening...")
+    process = []
+    for i in ports:
+        main_process = multiprocessing.Process(target=listen_process, args=('', i))
+        main_process.start()
+        process.append(main_process)
+        logging.info("Started processing on port {} ".format(i))
+
+    #TODO вынести в функцию или класс
+
+    # сообщаем планировщику, что все процессы запустились и можно продолжать работу
+    sock = socket.socket()
+    sock.connect(('', scheduler_port))
+    sock.send('port_created'.encode())
+    sock.close()
+    for i in process:
+        # ожидаем завершения процессов, иначе общие данные пропадут (Manager умирает при убивании основного процесса)
+        i.join()
+
